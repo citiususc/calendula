cc0fee15ed3909816b2a1bb9768fb2d4f3457666
Jose Ángel Piñeiro Souto <joseangel.pineiro@usc.es>
2016-09-22 18:39:48 +0200

    Update about activity

--- Calendula/src/main/java/es/usc/citius/servando/calendula/activities/AboutActivity.java
+++ Calendula/src/main/java/es/usc/citius/servando/calendula/activities/AboutActivity.java
@@ -15,7 +15,7 @@ public class AboutActivity extends CalendulaActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
-        setupToolbar("Open Source", getResources().getColor(R.color.dark_grey_home));
+        setupToolbar("About", getResources().getColor(R.color.dark_grey_home));
         setupStatusBar(getResources().getColor(R.color.dark_grey_home));
 
         if (savedInstanceState == null) {
@@ -26,9 +26,20 @@ public class AboutActivity extends CalendulaActivity {
                     .withAboutVersionShown(true)
                     .withLicenseShown(true)
                     .withLicenseDialog(true)
-                    .withAboutDescription(
-                            "Calendula es un proyecto de código abierto. Puedes contribuír con tus ideas y sugerencias en" +
-                                    "<br/><br/> <a href='https://github.com/citiususc/calendula'> https://github.com/citiususc/calendula </a>")
+                    .withAboutDescription("<br/><br/>Calendula está desarrollada por el Centro de Investigación en Tecnologías de la Información " +
+                            " de la USC (<a href='http://citius.usc.es'>CITIUS</a>)<br/><br/>Nuestro objetivo " +
+                            "es proporcionar a los pacientes y a las personas que se ocupan de ellos las herramientas " +
+                            "necesarias para mejorar su nivel de adherencia a la medicación." +
+                            "<br/><br/>Si quieres saber más visita nuestra web:" +
+                            "<br/> <a href='http://citius.usc.es/calendula'>citius.usc.es/calendula </a>" +
+                            "<br/><br/>Calendula es un proyecto de código abierto. Puedes contribuir con tus ideas y sugerencias " +
+                            "a través de nuestra página en Github o enviando un mensaje de correo electrónico:" +
+                            "<br/><a href='https://github.com/citiususc/calendula'>https://github.com/citiususc/calendula </a>" +
+                            "<br/><a href='mailto:citius.calendula@usc.es'>citius.calendula@usc.es</a>" +
+                            "<br/><br/>" +
+                            "Para este desarrollo hacemos uso de algunas librerías y utilidades de código abierto, que mostramos a continuacion:"
+
+                    )
                     .fragment();
 
             FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
