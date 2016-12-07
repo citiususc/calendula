e56973413844cb5d2be2af5963391e12b711c259
Álvaro Brey <alvaro.brv@gmail.com>
2016-12-07 14:22:08 +0100

    Remove useless menu in patients activity

--- Calendula/src/main/java/es/usc/citius/servando/calendula/activities/PatientsActivity.java
+++ Calendula/src/main/java/es/usc/citius/servando/calendula/activities/PatientsActivity.java
@@ -30,8 +30,6 @@ import android.support.v4.app.ActivityOptionsCompat;
 import android.support.v4.util.Pair;
 import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -63,28 +61,6 @@ public class PatientsActivity extends CalendulaActivity implements GridView.OnIt
     FloatingActionButton fab;
 
     List<Patient> patients = Collections.emptyList();
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_patients, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
