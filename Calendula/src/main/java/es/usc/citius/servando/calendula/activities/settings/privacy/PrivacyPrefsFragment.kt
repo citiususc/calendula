ad1183c2daefeb883957008e863c370a7645fe10
Álvaro Brey <alvaro.brv@gmail.com>
2018-02-05 13:21:11 +0100

    Implement PIN/fingerprint prefs again

---
+++ Calendula/src/main/java/es/usc/citius/servando/calendula/activities/settings/privacy/PrivacyPrefsFragment.kt
@@ -0,0 +1,152 @@
+/*
+ *    Calendula - An assistant for personal medication management.
+ *    Copyright (C) 2014-2018 CiTIUS - University of Santiago de Compostela
+ *
+ *    Calendula is free software; you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation; either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this software.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package es.usc.citius.servando.calendula.activities.settings.privacy
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.v14.preference.SwitchPreference
+import android.support.v7.preference.Preference
+import android.support.v7.preference.PreferenceFragmentCompat
+import com.github.javiersantos.materialstyleddialogs.MaterialStyledDialog
+import com.github.javiersantos.materialstyleddialogs.enums.Style
+import com.mikepenz.google_material_typeface_library.GoogleMaterial
+import es.usc.citius.servando.calendula.R
+import es.usc.citius.servando.calendula.pinlock.PinLockActivity
+import es.usc.citius.servando.calendula.pinlock.fingerprint.FingerprintHelper
+import es.usc.citius.servando.calendula.util.IconUtils
+import es.usc.citius.servando.calendula.util.LogUtil
+import es.usc.citius.servando.calendula.util.PreferenceKeys
+
+
+/**
+ * Instantiated via reflection, don't delete!
+ *
+ * Created by alvaro.brey.vilas on 1/02/18.
+ */
+class PrivacyPrefsFragment : PreferenceFragmentCompat(), PrivacyPrefsContract.View {
+
+
+    companion object {
+        private const val TAG = "PrivacyPrefsFragment"
+    }
+
+    override lateinit var presenter: PrivacyPrefsContract.Presenter
+
+    private val pinPref: Preference by lazy { findPreference(PreferenceKeys.UNLOCK_PIN.key()) }
+    private val fingerprintPref: SwitchPreference by lazy { findPreference(PreferenceKeys.FINGERPRINT_ENABLED.key()) as SwitchPreference }
+
+
+    override fun onResume() {
+        super.onResume()
+        presenter.start()
+    }
+
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        LogUtil.d(TAG, "onCreatePreferences called")
+        addPreferencesFromResource(R.xml.pref_privacy)
+
+        pinPref.setOnPreferenceClickListener {
+            presenter.onClickPINPref()
+            true
+        }
+
+        PrivacyPrefsPresenter(this, FingerprintHelper(context))
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        LogUtil.d(TAG, "onActivityResult() called")
+        presenter.onResult(requestCode, resultCode, data)
+    }
+
+
+    override fun recordPIN() {
+        val i = Intent(activity, PinLockActivity::class.java)
+        startActivityForResult(i, PinLockActivity.REQUEST_PIN)
+    }
+
+    override fun showPINOptions() {
+        MaterialStyledDialog.Builder(context)
+            .setTitle(getString(R.string.pin_actions_dialog_title))
+            .setDescription(R.string.pin_actions_dialog_message)
+            .setHeaderColor(R.color.android_green)
+            .setStyle(Style.HEADER_WITH_ICON)
+            .withDialogAnimation(true)
+            .setIcon(
+                IconUtils.icon(
+                    context,
+                    GoogleMaterial.Icon.gmd_key,
+                    R.color.white,
+                    100
+                )
+            )
+            .setPositiveText(R.string.pin_actions_dialog_delete)
+            .setNegativeText(R.string.pin_actions_dialog_modify)
+            .setNeutralText(R.string.pin_actions_dialog_cancel)
+            .onPositive { dialog, _ ->
+                presenter.onClickDeletePIN()
+                dialog.dismiss()
+            }
+            .onNegative { dialog, _ ->
+                presenter.onClickModifyPIN()
+                dialog.dismiss()
+            }
+            .onNeutral { dialog, _ -> dialog.dismiss() }
+            .show()
+    }
+
+    override fun showConfirmDeletePinChoice() {
+        MaterialStyledDialog.Builder(context)
+            .setTitle(getString(R.string.pin_delete_dialog_title))
+            .setDescription(R.string.pin_delete_dialog_message)
+            .setHeaderColor(R.color.android_red_dark)
+            .setStyle(Style.HEADER_WITH_ICON)
+            .withDialogAnimation(true)
+            .setIcon(
+                IconUtils.icon(
+                    context,
+                    GoogleMaterial.Icon.gmd_key,
+                    R.color.white,
+                    100
+                )
+            )
+            .setPositiveText(R.string.pin_actions_dialog_delete)
+            .setNegativeText(R.string.pin_actions_dialog_cancel)
+            .onPositive { dialog, _ ->
+                presenter.confirmDeletePIN()
+                dialog.dismiss()
+            }
+            .onNegative { dialog, _ -> dialog.dismiss() }
+            .show()
+    }
+
+    override fun setPINPrefText(pinPrefText: Int) {
+        pinPref.setSummary(pinPrefText)
+    }
+
+    override fun setFingerprintPrefEnabled(enabled: Boolean) {
+        fingerprintPref.isEnabled = enabled
+    }
+
+    override fun showEnableFingerprintDialog() {
+        // noop
+    }
+
+
+}
