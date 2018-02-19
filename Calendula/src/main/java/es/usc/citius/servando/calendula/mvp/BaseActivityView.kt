cc9e0b9f4114d03675ca4c02b59f02f0772e0add
Ángel Piñeiro Souto <joseangel.pineiro@usc.es>
2018-02-19 12:00:09 +0100

    Add base implementation for MVP activities

---
+++ Calendula/src/main/java/es/usc/citius/servando/calendula/mvp/BaseActivityView.kt
@@ -0,0 +1,38 @@
+/*
+ *    Calendula - An assistant for personal medication management.
+ *    Copyright (C) 2016 CITIUS - USC
+ *
+ *    Calendula is free software; you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation; either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this software.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package es.usc.citius.servando.calendula.mvp
+
+import android.os.Bundle
+import es.usc.citius.servando.calendula.CalendulaActivity
+
+
+abstract class BaseActivityView<in V : IView, out P : IPresenter<V>> : CalendulaActivity(), IView {
+
+    abstract val presenter: P
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        presenter.attachView(this as V)
+    }
+
+    override fun onDestroy() {
+        presenter.detachView()
+        super.onDestroy()
+    }
+}
